5e046edf38a2c3ecf4795828239bcfd9fab7348b
wazirwazir <151955963+wazirwazir@users.noreply.github.com>
2025-09-27 00:53:46 +0100

    update functionality

--- Js/script.js
+++ Js/script.js
@@ -1,3 +1,10 @@
+const saveCartedProducts = (data) => {
+  localStorage.setItem('allProducts', JSON.stringify(data)) 
+}
+if (!localStorage.getItem('allProducts')) {
+  saveCartedProducts([])
+}
+
 const navBtn = document.querySelector('.hamburger')
 const navBar = document.querySelector('#navBar')
 const cartContainer = document.querySelector('.cartContainer')
@@ -6,17 +13,23 @@ const eNumberInput = document.querySelector('#e-number')
 const ePinInput = document.querySelector('#e-Pin')
 const sectionName = document.querySelector('#section_name')
 let productsContainer = document.querySelector('.items_sold')
+const removeAllBtn = document.querySelector('.removeBtn')
 let products;
+let cartedItems;
 
-fetch('./data.json')
-.then(response => response.json())
-.then(data => {
-    console.log(data)
-    products = data.filter(item => item.category == sectionName.innerText.toLocaleLowerCase())
-    renderProducts(products)
-    itemQuantity()
-})
-.catch(err => console.error(err))
+if (productsContainer) {
+    fetch('./data.json')
+    .then(response => response.json())
+    .then(data => {
+        console.log(data)
+        products = data.filter(item => item.category == sectionName.innerText.toLocaleLowerCase())
+        renderProducts(products)
+        renderCartedProducts()
+        itemQuantity()
+    })
+    .catch(err => console.error(err))
+}
+
 
 
 const displayNav = () => {
@@ -30,6 +43,7 @@ const displayCart = () => {
     
     if (!navBar.classList.contains('hide')) {
         navBar.classList.toggle('hide')
+        cartCounter()
     }
     cartContainer.classList.toggle('hide')
 }
@@ -93,5 +107,109 @@ const itemQuantity = () => {
                 })
                 
             })
-            
+            addToCart()
         }
+const removeCartedItems = () => {
+  removeAllBtn.parentElement.parentElement.children[1].innerHTML = ''
+  removeCarted()
+  saveCartedProducts([])
+  const cartCount = document.querySelectorAll('.cart_count')
+  cartCount.forEach(counter => {
+    counter.innerHTML = ''
+  })
+}
+
+removeAllBtn.onclick = removeCartedItems
+const addToCart = () => {
+  const addToCartBtn = document.querySelectorAll('.cartBtn')
+  addToCartBtn.forEach(btn => {
+    let img = btn.parentElement.parentElement.previousElementSibling.children[0].src
+    let about = btn.parentElement.parentElement
+    let name = about.querySelector('h2').querySelector('span').innerText
+    let price = Number(about.querySelector('.item_price').querySelector('#itemPrice').innerText.replace(/,/g, ""))
+    
+    btn.addEventListener('click', () => {
+      let itemCount = Number(btn.parentElement.querySelector('.item_quantity').querySelector('.quantity_input').innerText)
+      let itemAdd = {
+        name: name,
+        price: price,
+        img: img,
+        quantity: itemCount,
+        cost: price * itemCount
+      } 
+      console.log(itemAdd)
+      
+      
+      updateCartedProducts(itemAdd)
+      cartedItems = getCartedProducts()
+      getSumOfCartItems(getCartedProducts(), 'quantity')
+      renderCartedProducts()
+    })
+  })
+}
+
+itemQuantity()
+
+const getCartedProducts = () => {
+  return JSON.parse(localStorage.getItem('allProducts')) || [];
+}
+
+/* const addCartedProducts = (item) => {
+  let store = getCartedProducts()
+  store.push(item);
+  saveCartedProducts(store)
+} */
+const updateCartedProducts = (product) => {
+  let store = getCartedProducts();
+  let existing = store.find(item => item.name == product.name)
+
+  if (existing) {
+    existing.quantity = product.quantity
+    existing.cost = product.cost
+  } else {
+    store.push(product)
+  }
+  saveCartedProducts(store)
+  getCartedProducts()
+}
+
+const removeCarted = () => {
+  localStorage.removeItem('allProducts')
+}
+
+
+const renderCartedProducts = () => {
+  let cart = getCartedProducts()
+  const cartHolder = document.querySelectorAll('.carted_items')
+  cartHolder.forEach(cartHolder => {
+    cartHolder.innerHTML = cart.map(item => {
+      return `<div class="carted">
+                <img src="${item.img}" alt="carted_img">
+                  <div class="carted_details">
+                    <p>${item.name}</p>
+                    <p>$<span>${item.price.toLocaleString()}</span></p>
+                  </div>
+                  <div class="item_quantity">
+                    <p class="quantity_input">X${item.quantity}</p>
+                  </div>
+              </div>
+      `
+    }).join('')
+  })
+  
+  cartCounter()
+}
+const getSumOfCartItems = (array, key) => {  
+  
+  return array.reduce((sum, item) => {
+    return sum + (item[key] || 0);
+  }, 0)
+}
+
+const cartCounter = () => {
+  const cartCount = document.querySelectorAll('.cart_count')
+  cartCount.forEach(counter => {
+    counter.innerHTML = getSumOfCartItems(getCartedProducts(), 'quantity')
+  })
+}
+window.onload = renderCartedProducts
